5fe73478be4a647271b6571e107e1e62af092c52
caparra92 <caparra92@gmail.com>
2020-06-25 19:26:40 -0300

    Adding disk and print icons for test form

---
+++ src/components/Disk.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    class="w-10"
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+    style="background-size:initial;background-repeat-y:initial;background-repeat-x:initial;background-position-y:initial;background-position-x:initial;background-origin:initial;background-image:initial;background-color:#fff;background-clip:initial;background-attachment:initial;animation-play-state:paused"
+  >
+    <g
+      class="ldl-scale"
+      style="transform-origin:50% 50%;animation-play-state:paused"
+      stroke-miterlimit="10"
+      stroke-width="3.5"
+      stroke="#333"
+      stroke-linecap="round"
+    >
+      <path
+        fill="#f47e60"
+        d="M52.72 8.8H12.16a3.38 3.38 0 00-3.36 3.36v55.68a3.38 3.38 0 003.36 3.36h55.68a3.38 3.38 0 003.36-3.36V27.28c0-1.68-.64-3.36-1.92-4.56l-12.08-12C56 9.44 54.4 8.8 52.72 8.8z"
+        style="animation-play-state:paused"
+        stroke-width="2.8000000000000003"
+      />
+      <path
+        fill="#e0e0e0"
+        d="M19.84 71.2h40.4V45.04c0-2.8-2.24-5.12-5.12-5.12H24.88c-2.8 0-5.12 2.24-5.12 5.12V71.2zM50.08 8.8H19.84v17.76c0 1.44 1.2 2.64 2.64 2.64h25.04c1.44 0 2.64-1.2 2.64-2.64V8.8z"
+        style="animation-play-state:paused"
+        stroke-width="2.8000000000000003"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/Printer.vue
@@ -0,0 +1,67 @@
+<template>
+  <svg
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+    style="background-size:initial;background-repeat-y:initial;background-repeat-x:initial;background-position-y:initial;background-position-x:initial;background-origin:initial;background-image:initial;background-color:#fff;background-clip:initial;background-attachment:initial;animation-play-state:paused"
+  >
+    <g
+      class="ldl-scale"
+      style="transform-origin:50% 50%;animation-play-state:paused"
+      transform="scale(.8)"
+    >
+      <path
+        stroke-miterlimit="10"
+        stroke-linejoin="round"
+        stroke-width="4.5"
+        stroke="#221e1f"
+        fill="#666"
+        d="M90 71.2H10V53.6c0-5.2 4.2-9.4 9.4-9.4h61.2c5.2 0 9.4 4.2 9.4 9.4v17.6z"
+        style="animation-play-state:paused"
+      />
+      <path
+        d="M77.5 87.3H26.7V12.7h28.5L77.5 35z"
+        stroke-miterlimit="10"
+        stroke-linejoin="round"
+        stroke-width="4.5"
+        stroke="#221e1f"
+        fill="#fff"
+        style="animation-play-state:paused"
+      />
+      <path
+        stroke-miterlimit="10"
+        stroke-linejoin="round"
+        stroke-width="4.5"
+        stroke="#221e1f"
+        fill="#666"
+        d="M81.5 44.2h-4v7.4H26.7v-7.4h-8.3c-4.7 0-8.5 3.8-8.5 8.5v18.6h16.7v-5.7h50.8v5.7H90V52.7c0-4.7-3.8-8.5-8.5-8.5z"
+        style="animation-play-state:paused"
+      />
+      <circle fill="#1bd112" r="2.8" cy="53" cx="18.2" style="animation-play-state:paused" />
+      <path
+        d="M35.6 65.6H44v21.8h-8.4zM52.3 65.6h8.4v21.8h-8.4zM69.1 65.6h8.4v21.8h-8.4z"
+        stroke-miterlimit="10"
+        stroke-linejoin="round"
+        stroke-width="4.5"
+        stroke="#221e1f"
+        fill="#fff"
+        style="animation-play-state:paused"
+      />
+      <path
+        d="M77.5 35L55.2 12.7V35z"
+        stroke-miterlimit="10"
+        stroke-linejoin="round"
+        stroke-width="4.5"
+        stroke="#221e1f"
+        fill="#e0e0e0"
+        style="animation-play-state:paused"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
